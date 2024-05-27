e23f4b3071621e43ae0ee7cbcd58c64d7215dc9d
robert.grellmann <robert.grellmann@rossmann.de>
2024-05-27 11:10:17 +0200

    more tests

--- tests/RockPaperScissorTest.php
+++ tests/RockPaperScissorTest.php
@@ -5,15 +5,26 @@ declare(strict_types = 1);
 namespace KataTests;
 
 use Kata\RockPaperScissor;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 
 class RockPaperScissorTest extends TestCase
 {
-    public function test_that_game_is_a_draw(): void
+    public static function draw()
+    {
+        yield "both play rock" => ['rock'];
+        yield "both play paper" => ['paper'];
+        yield "both play scissors" => ['scissors'];
+    }
+
+    #[Test]
+    #[DataProvider("draw")]
+    public function test_that_game_is_a_draw($option): void
     {
         $game = new RockPaperScissor();
-        $result = $game->playRound('','');
+        $result = $game->playRound($option,$option);
 
         self::assertSame('draw', $result);
     }
@@ -24,6 +35,14 @@ class RockPaperScissorTest extends TestCase
         $result = $game->playRound('rock','scissors');
 
         self::assertSame('rock', $result);
+    }
+
+    public function test_that_paper_beats_rock(): void
+    {
+        $game = new RockPaperScissor();
+        $result = $game->playRound('paper','rock');
+
+        self::assertSame('paper', $result);
     }
 
 
